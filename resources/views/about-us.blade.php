8ab5a62e30d44662160b0ea34289229e1099850f
Coreyyy77 <99870545+Coreyyy77@users.noreply.github.com>
2022-03-27 17:12:10 +0800

    done about us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -65,17 +65,20 @@
     <div class="text-center mx-auto font-bold text-9xl mt-20">About Us</div>
 
     <div class="w-full md:max-w-3xl lg:max-w-4xl xl:max-w-5xl mx-auto sm:my-2 md:my-5 lg:my-10 overflow-x-hidden routeMinHeight">
-    <h1 class="text-center ">Our story</h1>
+    <h1 class="text-center ">Our Story</h1>
     </br>
-    <p class="">Supreme Computer Systems is a Malaysia company focused on providing quality services and solutions to satisfy customers. Founded in  21st June 1994 in Kuantan, Pahang, Supreme Computer Systems has fulfilled and satisfied all customer with excellent services to this date. Supreme Computer Systems has sold an array of Softwares and Hardwares to many
-        companies and customers, fulfilling their needs for technological advancement.</p>
+    <p class="">Supreme Computer Systems is a Malaysia IT company focused on providing quality services and solutions to satisfy customers needs. Founded in  22nd July 2001 in Kuantan, Pahang, Supreme Computer Systems has fulfilled and satisfied all customer with excellent services to this date. From the day we start our business, we only been receiving nothing else but complimenets for our top-notch customer service and after sales service. Supreme Computer Systems has sold a wide variety of Softwares and Hardwares to many companies and customers, fulfilling their needs for technological advancement.</p>
     </br>
-    <h1 class="text-center">Our Mission</h1>
+    <h1 class="text-center">Our Goal</h1>
     </br>
-    <p>“SCS ‘s mission is to enhance the competitive position of our clients by equipping them with state-of-the art IT solution and continuously servicing them with a full range of professional and quality services.
+    <p>“SCS ‘s goal is to improve and enhance the competitive position of our clients by providing them what they want and desire and continuously servicing them with a full range of professional and quality services. We prioritize customer satisfaction by providing guidance and solutions to all our customer's needs, allowing you to prioritize on your business objectives.
 
-        To achieve this, SCS must be one of the largest independent IT services companies, growing faster than the industry average by operating profitably on a global basis and maintaining an increasing core of loyal,
-        satisfied clients and dedicated, well-trained and motivated staff.”</p>
+       Although we are founded in the early days, our team is still growing and aiming to achieve more in the future. We are a team equipped with highly motavated, well-trained and dedicated staffs.”</p>
+    </br>
+	<h1 class="text-center">Why us ?</h1>
+</br>
+    <p>SCS is one of the leading online e-tailers with presences in 3 countries; Australia, Malaysia and Singapore.We offer a wide selection of top brand IT products with the most competitive prices in the local market. In 3 years, we were awarded the Malaysia Top IT Product E-Commerce reseller in Lazada and Shopee and achieved 5 stars rating from over 30,000 reviews.</p>
+
     </div>
 </body>
 </html>
