1cfc52684a3afeae877effbb3d17426ea949f97a
freshnoob1e <86183941+freshnoob1e@users.noreply.github.com>
2022-04-02 16:46:07 +0800

    pc builder init

--- resources/views/pc-builder.blade.php
+++ resources/views/pc-builder.blade.php
@@ -1,21 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        @livewireStyles
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    </head>
-    <body class="antialiased">
-        {{-- aaaaaaaaaaaaa --}}
-        <div class="text-center mx-auto font-bold text-9xl">PC Builder</div>
-    </body>
+    <!-- Styles -->
+    @livewireStyles
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+</head>
+
+<body class="antialiased">
+    @include('components.navbar')
+    <div class="max-w-7xl min-h-screen mx-auto">
+        <div class="border rounded-xl mx-auto mt-24 p-5">
+            <div class="text-2xl font-semibold">
+                PC Builder
+            </div>
+            <div class="mt-5">
+                <livewire:p-c-builder>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
